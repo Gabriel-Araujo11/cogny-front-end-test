1ab0a6ab519f50cd1aebb2ef1f0c2eeaec65ac1f
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2024-08-09 17:53:54 -0300

    feat: create cart item component

---
+++ web/src/components/cartItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box, Text, Image } from "@chakra-ui/react";
+
+const CartItem = ({ item }) => {
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      p={4}
+      borderWidth="1px"
+      borderRadius="lg"
+      mb={4}
+    >
+      <Image
+        src={item.product.image}
+        alt={item.product.name}
+        boxSize="100px"
+        objectFit="cover"
+      />
+      <Box ml={4}>
+        <Text fontWeight="bold">"{item.product.name}"</Text>
+        <Text>Quantidade: "{item.quantity}"</Text>
+        <Text>Preço: $"{item.product.price * item.quantity}"</Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default CartItem;
