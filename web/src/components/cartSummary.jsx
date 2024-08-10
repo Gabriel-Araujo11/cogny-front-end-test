45c5eea0319ed59b17e023826f2afeaad0fd9fa5
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2024-08-10 00:47:30 -0300

    fix: refactor logic cart void

--- web/src/components/cartSummary.jsx
+++ web/src/components/cartSummary.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { Box, Button, Text, Flex } from "@chakra-ui/react";
 import { CartContext } from "../context/cartContext";
 import AlertMessage from "../utils/Alert";
+import { Link } from "react-router-dom";
 
 export default function CartSummary({ total }) {
   const { resetCart } = useContext(CartContext);
@@ -34,24 +35,40 @@ export default function CartSummary({ total }) {
           onClose={() => setAlert(null)}
         />
       )}
-      <Flex justifyContent="space-between" alignItems="center">
-        <Button
-          colorScheme="teal"
-          onClick={handleCheckout}
-          _hover={{ bg: "#c62c4a" }}
-          bg="#F8375D"
-        >
-          FINALIZAR PEDIDO
-        </Button>
-        <Flex alignItems="center">
-          <Text color="gray.500" mr={2} fontWeight="bold">
-            TOTAL:
-          </Text>
-          <Text fontWeight="bold" color="black" fontSize="30px" mb={4} mt={2}>
-            R$ {total}
-          </Text>
+      {!total ? (
+        <Box>
+          <Text mb={5}>O Carrinho está vazio. </Text>
+          <Link to="/">
+            <Button
+              colorScheme="teal"
+              onClick={handleCheckout}
+              _hover={{ bg: "#c62c4a" }}
+              bg="#F8375D"
+            >
+              VOLTAR
+            </Button>
+          </Link>
+        </Box>
+      ) : (
+        <Flex justifyContent="space-between" alignItems="center">
+          <Button
+            colorScheme="teal"
+            onClick={handleCheckout}
+            _hover={{ bg: "#c62c4a" }}
+            bg="#F8375D"
+          >
+            FINALIZAR PEDIDO
+          </Button>
+          <Flex alignItems="center">
+            <Text color="gray.500" mr={2} fontWeight="bold">
+              TOTAL:
+            </Text>
+            <Text fontWeight="bold" color="black" fontSize="30px" mb={4} mt={2}>
+              R$ {total}
+            </Text>
+          </Flex>
         </Flex>
-      </Flex>
+      )}
     </Box>
   );
 }
